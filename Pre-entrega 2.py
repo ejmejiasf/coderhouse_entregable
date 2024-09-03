3b50f903abe9edcfc76af96478f46a4762a2b5d4
ejmejiasf <ejmejiasf@gmail.com>
2024-09-03 02:51:11 +0200

    Main.py

--- Pre-entrega 2.py
+++ Pre-entrega 2.py
@@ -1,6 +1,8 @@
 import requests
+import json
 import pandas as pd
-from sqlalchemy import create_engine
+import psycopg2
+from psycopg2.extras import execute_values
 
 # Paso 1: Extrayendo de Datos desde la API
 url = 'https://api.football-data.org/v2/competitions/PL/matches'
@@ -13,9 +15,62 @@ df.drop_duplicates(inplace=True)
 df.dropna(inplace=True)
 df['main.temp'] = df['main.temp'].astype(float)
 
-# Paso 3: Cargando de Datos en Amazon Redshift
-# Configurando la conexión a Redshift
-engine = create_engine('postgresql+psycopg2://ejmejiasf_coderhouse:pAjkuk033c@redshift-cluster-https://api.football-data.org/v2/competitions/PL/matches')
+# Configuración de conexión a Redshift
+host = 'tu_host_redshift'
+dbname = 'tu_nombre_base_datos'
+user = 'ejmejiasf_coderhouse'
+password = 'pAjkuk033c'
+port = '5439'
 
-# Carga los datos en Redshift
-df.to_sql('mi tabla', engine, if_exists='replace', index=False)
+try:
+    conn = psycopg2.connect(
+        host=host,
+        dbname=dbname,
+        user=user,
+        password=password,
+        port=port
+    )
+    print("Connected to Redshift successfully!")
+except Exception as e:
+    print("Unable to connect to Redshift.")
+    print(e)
+
+# Función para cargar los datos
+def cargar_en_redshift(conn, table_name, dataframe):
+    dtypes = dataframe.dtypes
+    cols = list(dtypes.index)
+    tipos = list(dtypes.values)
+    type_map = {'int64': 'INT', 'float64': 'FLOAT', 'object': 'VARCHAR(50)'}
+    sql_dtypes = [type_map[str(dtype)] for dtype in tipos]
+    
+    # Definir formato SQL VARIABLE TIPO_DATO
+    column_defs = [f"{name} {data_type}" for name, data_type in zip(cols, sql_dtypes)]
+    
+    # Crear la tabla si no existe
+    table_schema = f"""
+        CREATE TABLE IF NOT EXISTS {table_name} (
+            {', '.join(column_defs)}
+        );
+    """
+    
+    # Crear la tabla
+    cur = conn.cursor()
+    cur.execute(table_schema)
+    
+    # Generar los valores a insertar
+    values = [tuple(x) for x in dataframe.to_numpy()]
+    
+    # Definir el INSERT
+    insert_sql = f"INSERT INTO {table_name} ({', '.join(cols)}) VALUES %s"
+    
+    # Insertar los datos
+    cur.execute("BEGIN")
+    execute_values(cur, insert_sql, values)
+    cur.execute("COMMIT")
+    print('Proceso terminado')
+    cur.close()
+
+# Carga de datos en RedShift
+cargar_en_redshift(conn=conn, table_name='mi_tabla', dataframe=df)
+
+conn.close()
