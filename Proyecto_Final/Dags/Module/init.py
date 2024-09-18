eb3c775e9bd5d28da840405d135339341d392541
ejmejiasf <ejmejiasf@gmail.com>
2024-09-18 22:07:12 +0200

    Create init.py

---
+++ Proyecto_Final/Dags/Module/init.py
@@ -0,0 +1,5 @@
+from .utils import get_defaultairflow_args
+from .data_extract import extraccion_data
+from .data_load import carga_data
+from .data_transform import transformando_data
+from .mail_sender import envio_mail
